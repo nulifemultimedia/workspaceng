ed0ecd3f7f1a7b77e4f4f5b91157b87aba086c70
nulifemultimedia <nulifentertainment@gmail.com>
2020-02-27 14:26:06 +0100

    Feb 27 2020

--- js/myjquery.js
+++ js/myjquery.js
@@ -1,6 +1,10 @@
-$(document).ready(function(){
-   
-    $(".intro-item").hide().slideDown(1000);
-    
-    
+//$(document).ready(function(){
+//   
+//    $(".intro-item").hide().slideDown(1000);
+//    
+//    
+//});
+
+document.querySelector('.img__btn').addEventListener('click', function() {
+  document.querySelector('.cont').classList.toggle('s--signup');
 });
